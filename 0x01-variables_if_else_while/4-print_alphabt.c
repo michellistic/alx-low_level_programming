1653ca9f1a7fe317fd0b8844edee9ee2ee041855
michellistic <enobongukpong73@yahoo.com>
2023-04-22 11:47:10 -0700

    gg

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Pirnts all letters except q and e
+ * Retrun: Always (success)
+ */
+
+int main(void)
+{
+	char low, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (low = 'a', low <= 'z', low++)
+	{
+		if (low != e && low != q)
+			putchar(low);
+	}
+	putchar ('\n');
+	return (0);
+}
