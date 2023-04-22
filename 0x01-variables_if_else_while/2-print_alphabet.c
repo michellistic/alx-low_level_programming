37dc2cd1a4da383b2fea120b4ba715268bfefe05
michellistic <enobongukpong73@yahoo.com>
2023-04-22 11:03:14 -0700

    gg

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/** 
+ * main - Returns the alphabet
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
