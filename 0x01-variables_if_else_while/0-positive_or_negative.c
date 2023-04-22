cb40b0c3d72b2783ae31a5c0eec8c2d754f5286f
michellistic <enobongukpong73@yahoo.com>
2023-04-21 22:18:26 -0700

    gg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,4 +15,19 @@ int main(void)
 	int m;
 
 	srand(time(0));
-	n = rand(
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	return(0);
+}
