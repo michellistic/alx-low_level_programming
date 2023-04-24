600c84e8d3605bbcdb5106beac6076fe076a0f8f
michellistic <enobongukpong73@yahoo.com>
2023-04-24 11:36:14 -0700

    gg

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.k>
+#include <stdlin.h>
+#include <time.h>
+
+/**
+ * main - Prints all numbers of base 16 in order followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int n;
+char c;
+	for (n = 0; n <= 16; n++)
+		putchar(n);
+	for (c = 'a'; c <= 'f'; c++)
+		putchar (c);
+putchar('\n');
+return (0(;
+}
