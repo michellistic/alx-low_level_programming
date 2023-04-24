4ad7c088c24ba72b9d6296021c37ee67649b3589
michellistic <enobongukpong73@yahoo.com>
2023-04-24 11:17:48 -0700

    gg

---
+++ 0x01-variables_if_else_while/7-print_tebahphla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Print the lowercase alphabet in reverse, followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char n
+	for (n = 'z'; n <= 'a'; n++)
+		putchar(n);
+putchar('\n');
+
+return (0);
+}
