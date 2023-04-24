c6db5468ff80cdc0a3dd61be5ea764e05d1612c1
michellistic <enobongukpong73@yahoo.com>
2023-04-24 11:11:00 -0700

    gg

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <time.h>
 
-/** main - Prints all single digit numbers of base 10 starting from 0
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0
  * Return - Always 0 (Success)
  */
 
