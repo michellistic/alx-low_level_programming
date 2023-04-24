1b368ac0cd0a6b6ee327eb93821e2830efba3d6f
michellistic <enobongukpong73@yahoo.com>
2023-04-24 12:03:02 -0700

    gg

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,12 @@ int main(void)
 int n;
 	for (n = '0'; n <= '9'; n++)
 		putchar(n);
-	
+
 	if (n != '9')
-		{
-			putchar(',');
-			putchar(' ');
-		}
+	{
+		putchar(',');
+		putchar(' ');
+	}
 	putchar('\n');
 return (0);
 }
