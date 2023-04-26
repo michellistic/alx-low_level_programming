90625b610a324779d0bb638636cc9812a9f7f9f6
michellistic <enobongukpong73@yahoo.com>
2023-04-25 20:04:20 -0700

    gg

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign(int n) - Prints the sign of a number
+ * @n: integer to be checked
+ * Return: 1 if positive, -1 if 
+ */
+
+int print_sign(int n)
+
+{
+	if (n >= 1)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+}
