70bb7dd97dc2cfcb39c5970a954ebbcd5c52cc9b
michellistic <enobongukpong73@yahoo.com>
2023-04-26 15:52:53 -0700

    gg

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
 /**
  * _abs - computes the absolute value of an integer
  * @c: number to be computed
- * Retunr: Absolute value of number or zero
+ * Return: Absolute value of number or zero
  */
 
 int _abs(int c)
