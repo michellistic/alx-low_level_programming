99b6140ee4d5b05d18d5245e5d55e99c178a0957
michellistic <enobongukpong73@yahoo.com>
2023-04-26 16:45:46 -0700

    m

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				_putchar("%d", b);
+				_putchar("%d", n);
 				_putchar("\n");
 				break;
 			}
@@ -30,8 +30,8 @@ void print_to_98(int n)
 		{
 			if (n == 98)
 			{
-				printf("%d", n);
-				printf("\n");
+				_putchar("%d", n);
+				_putchar("\n");
 				break;
 			}
 		}
