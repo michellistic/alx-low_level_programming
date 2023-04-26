50564cc2743a7d84437570760c75c3baf6630412
michellistic <enobongukpong73@yahoo.com>
2023-04-25 18:00:56 -0700

    gg

---
+++ 0x02-functions_nested_loops/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Prints the alphabet ten times
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+	while (i <= 9)
+	{
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c)
+	}
+	putchar('\n');
+	i++;
+	}
+}
