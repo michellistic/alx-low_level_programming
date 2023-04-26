b1405cdfbcb4facf86890ae9ec31b4f434b61612
michellistic <enobongukpong73@yahoo.com>
2023-04-25 17:31:21 -0700

    gg

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/** 
+ * main - Prints the alphabet in lowercase followed by a newline
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
