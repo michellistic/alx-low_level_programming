54b67fb1b2442631832b4a5ab21432639a67812f
michellistic <enobongukpong73@yahoo.com>
2023-04-25 20:43:30 -0700

    gg

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints last digit
+ * @n: number to be checked
+ * Return: Last
+ */
+
+int print_last_digit{int}
+{
+	int last;
+
+	last = n % 10;
+	if (last < 0)
+		last = last * -1;
+	_putchar(last + '0');
+	return (last);
+}
