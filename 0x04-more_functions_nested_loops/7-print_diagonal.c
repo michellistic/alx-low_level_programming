add40c02fc7ae9ec58a76183cd5547c380d0172f
michellistic <enobongukpong73@yahoo.com>
2023-04-28 13:27:13 -0700

    m

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal
+ * @n: number of times \ should be printed
+ */
+
+void print_diagonal(int n)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < x; y++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
