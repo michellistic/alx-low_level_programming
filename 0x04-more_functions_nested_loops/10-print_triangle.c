ba6c402429381bb8f7817e5eefddb6c8a6eb7aee
michellistic <enobongukpong73@yahoo.com>
2023-04-28 14:06:32 -0700

    m

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,13 +14,13 @@ void print_triangle(int size)
 	{
 		_putchar('\n');
 	}
-	for (x = 0; x < size; x++);
+	for (x = 0; x < size; x++)
 	{
 		for (y = size - x; y > 1; y--)
 		{
 			_putchar(32);
 		}
-		for (z = 0; z <= x; z++);
+		for (z = 0; z <= x; z++)
 		{
 			_putchar(35);
 		}
