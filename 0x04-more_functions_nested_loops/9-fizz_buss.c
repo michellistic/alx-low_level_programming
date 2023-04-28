d4c77dc5085e23836af6acf3d5d20726728a8da2
michellistic <enobongukpong73@yahoo.com>
2023-04-28 13:47:02 -0700

    m

---
+++ 0x04-more_functions_nested_loops/9-fizz_buss.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints the FizzBuzz program
+ * Return: the fizzbuzz program
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d");
+		}
+	}
+	printf('\n');
+	return (0);
+}
