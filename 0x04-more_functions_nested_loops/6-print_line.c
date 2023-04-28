1af5efa278098c71dc23b027378cdf64bd70873a
michellistic <enobongukpong73@yahoo.com>
2023-04-28 13:08:11 -0700

    m

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - what can i say.
+ * @n: number of times the character _ is printed
+ * Eeturn: hopefully a straight line
+ */
+
+void print_line(void)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
