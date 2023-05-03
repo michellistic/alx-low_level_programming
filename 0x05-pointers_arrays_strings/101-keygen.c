b5d43f92f0840219cf22e6efe4d1cde3f09e73c9
michellistic <enobongukpong73@yahoo.com>
2023-05-03 10:04:13 -0700

    m

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,1 +1,16 @@
-#inclu
+#include <stdio.h>
+#include "main.h"
+#include <time.h>
+
+/**
+ * main - prints a program that generates valid passwords for 101-crackme
+ * Return: 0
+ */
+
+int main(void)
+{
+	char password[84];
+	int index = 0; sum = 0; half_diff1; half_diff2;
+
+	srand(time(0));
+}
