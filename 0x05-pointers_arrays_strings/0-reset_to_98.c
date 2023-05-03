a2cb01793697474cd72b9f55af47ccfd55743c08
michellistic <enobongukpong73@yahoo.com>
2023-05-02 18:45:21 -0700

    m

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to int and updates it to 98
+ * @n: integer to be tking
+ * ReturnL nothing
+ */
+
+void reset_to_98(int *n)
+{
+	int n;
+
+	*m = 98;
+}
