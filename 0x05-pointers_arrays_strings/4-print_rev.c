f3ae00ab717108171ce920bf6a1a1aae62caea91
michellistic <enobongukpong73@yahoo.com>
2023-05-02 19:37:35 -0700

    m

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: string to be printed in reverse
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int c = 0;
+
+	while(s[c] != 0)
+	{
+		c++;
+	}
+	for (c -= 1; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}
