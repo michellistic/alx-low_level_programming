341329be31ab4236fc4f5ce39acded9d64eb0d27
michellistic <enobongukpong73@yahoo.com>
2023-05-04 15:33:28 -0700

    m

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,7 +17,7 @@ char *cap_string(char *str)
 	if (str[index - 1] == ' ' || str[index - 1] == '\t' || str[index - 1] == '\n' || str[index - 1] == ',' || str[index - 1] == ';' || str[index - 1] == '.' || str[index - 1] == '!' || str[index - 1] == '?' || str[index - 1] == '"' || str[index - 1] == '{' || str[index - 1] == ')' || str[index - 1] == '{' || str[index - 1] == '}' || index == 0)
 	{
 		str[index] -= 32;
-		index++;
 	}
+	index++;
 	return (str);
 }
