07b36c03290e0232aae05e24aa01201a127d3533
michellistic <enobongukpong73@yahoo.com>
2023-05-04 15:53:27 -0700

    m

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,7 +3,7 @@
 /**
  * leet - encode into 1337speak
  * @n: input value
- * Retunr: n
+ * Return: n
  */
 
 char *leet(char *n)
@@ -22,4 +22,5 @@ char *leet(char *n)
 			}
 		}
 	}
+	return (n);
 }
