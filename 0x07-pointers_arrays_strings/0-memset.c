3265a43e106b4141163dc9ec9800aa44c3accece
michellistic <enobongukpong73@yahoo.com>
2023-05-08 18:26:07 -0700

    m

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - entry point
+ * @s: variable 
+ * @b: byte
+ * @n: number
+ * Return: 0
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; n++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
