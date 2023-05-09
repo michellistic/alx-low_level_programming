ca634eb73af3b12b9bd92f4be405a13ea0779ff9
michellistic <enobongukpong73@yahoo.com>
2023-05-08 18:44:31 -0700

    m

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,7 +2,7 @@
 
 /**
  * _memset - entry point
- * @s: variable 
+ * @s: variable
  * @b: byte
  * @n: number
  * Return: 0
@@ -12,7 +12,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; n++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
