bd9fedfc7978477480da9a39a1f8c0b917da147a
michellistic <enobongukpong73@yahoo.com>
2023-09-01 10:20:38 -0700

    m

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
-
-# Compile each .c file into a corresponding .o file
-for source_file in *.c; do
-    object_file="${source_file%.c}.o"
-    if ! gcc -Wall -Wextra -Werror -pedantic -c "$source_file" -o "$object_file"; then
-        echo "Error compiling $source_file"
-        exit 1
-    fi
-done
-
-# Create the static library from all the .o files
+gcc -Wall -Wextra -Werror -pedantic -c *.c
 ar rc liball.a *.o
-
-# Clean up the .o files
-rm -f *.o
-
-echo "Static library liball.a created."
